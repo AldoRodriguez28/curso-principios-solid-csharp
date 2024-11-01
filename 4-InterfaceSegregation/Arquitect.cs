f3c7b0a03f4965810e3a8415e27b1992d4f57cff
AldoRodriguez28 <arodriguez.ronquillo@hotmail.com>
2024-11-01 09:48:10 -0600

    terminando curso y suibiendo cambios

---
+++ 4-InterfaceSegregation/Arquitect.cs
@@ -0,0 +1,33 @@
+namespace InterfaceSegregation
+{
+
+    public class Arquitect : IActivities
+    {
+        public Arquitect(){}
+
+        public void Comunicate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Design()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Develop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Plan()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Test()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
